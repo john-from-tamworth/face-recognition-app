3a1e199027553ec9ce55b50550edc1214652371d
John Hands <johnhands@Johns-MacBook-Pro.local>
2020-02-10 22:09:32 +0000

    Updates

---
+++ precache-manifest.371504fd66e84169de7cc234bd193c0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e4c6d8c413b400c07d74d2f821c9664",
+    "url": "/face-recognition-app/index.html"
+  },
+  {
+    "revision": "b9d3da94d7f93bb0ef5d",
+    "url": "/face-recognition-app/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "95b1cb957722600dcbef",
+    "url": "/face-recognition-app/static/css/main.25412e15.chunk.css"
+  },
+  {
+    "revision": "b9d3da94d7f93bb0ef5d",
+    "url": "/face-recognition-app/static/js/2.1c06ecf8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/face-recognition-app/static/js/2.1c06ecf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95b1cb957722600dcbef",
+    "url": "/face-recognition-app/static/js/main.93aeb565.chunk.js"
+  },
+  {
+    "revision": "214d99cca5e47214cb55",
+    "url": "/face-recognition-app/static/js/runtime-main.a7b829c7.js"
+  }
+]);
