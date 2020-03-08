326707dbff0d93c6a1eee63a461c2e614e9d2dbb
John Hands <johnhands@Johns-MacBook-Pro.local>
2020-03-08 02:32:15 +0000

    corrected image count

--- src/App.js
+++ src/App.js
@@ -89,7 +89,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('https://tranquil-ocean-06577.herokuapp.com:3000/image', {
+          fetch('https://tranquil-ocean-06577.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
