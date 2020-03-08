54f8597c315553eb1b02b3bff02b5bbbb47de067
John Hands <johnhands@Johns-MacBook-Pro.local>
2020-03-08 02:56:24 +0000

    corrected sign in

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch('https://tranquil-ocean-06577.herokuapp.com//signin', {
+    fetch('https://tranquil-ocean-06577.herokuapp.com/signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
